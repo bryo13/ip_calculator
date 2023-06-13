85f0622faf47024cb79ebcf36fc9b047870e3856
Brian Tum <brian.tum10@yahoo.com>
2023-06-13 15:44:55 +0300

    redo classify_ip function

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 import sys
-import re
+import ipaddress
 
 ip_file = sys.argv[1]
 
@@ -10,33 +10,22 @@ class Calculator:
     def __init__(self, filename) -> None:
         self.filename = filename
 
-    def private_or_public_ip(self,ip):
-        private_regex = re.compile(
-            r"^(10|172\.16|192\.168)\.([0-9]{1,3}\.){2}([0-9]{1,3})$"
-        )
-        public_regex = re.compile(
-            r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))$"
-        )
-        loopnack_regex = re.compile(r"^(127)\.([0-9]{1,3}\.){2}([0-9]{1,3})$")
-
-        if private_regex.match(ip):
-            print(f"{ip} - private ip")
-            return "private"
-        elif public_regex.match(ip):
-            print(f"{ip} - public ip")
-            return "public"
-        elif loopnack_regex.match(ip):
-            print(f"{ip} - loopback ip")
-            return "loopback"
-        else:
-            print("check your ip address!")
-            return "error"
-
-    def read_ips(self):
+    def classify_ip(self):
         with open(self.filename, encoding="utf-8") as ip:
             for lines in ip:
-                self.private_or_public_ip(lines)
+                ip_address = ipaddress.ip_address(lines.removesuffix("\n"))
+
+                if ip_address.is_private:
+                    print(f"{ip_address} is a private address")
+                elif ip_address.is_global:
+                    print(f"{ip_address} is a public address")
+                elif ip_address.is_loopback:
+                    print(f"{ip_address} is a loopback address")
+                elif ip_address.is_reserved:
+                    print(f"{ip_address} is a reserved address")
+                else:
+                    print("check your ip address")
 
 
 ips = Calculator("ips.txt")
-ips.read_ips()
+ips.classify_ip()
